87585aba16bd1673e274ed860b86021533e9bfe5
Ariesjenzelle <88218684+Ariesjenzelle@users.noreply.github.com>
2021-12-17 15:02:03 +0800

    Finals

---
+++ FINALS/Assets/Script/Savedata.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Savedata : MonoBehaviour
+{
+    public InputField textBox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void clickSave()
+    {
+        PlayerPrefs.SetString("username", textBox.text);
+        SceneManager.LoadScene("SecondScene");
+    }
+}
